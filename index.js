a3a09fe1d2b2d665698a1ba9e5ce199dc8e161f0
Marcin Szczepanski <mszczepanski@atlassian.com>
2023-07-05 14:41:50 +1000

    Try to get an agnostic repro working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { LMDBCache } = require("@parcel/cache");
+const db = require("lmdb");
 const WorkerFarm = require("@parcel/workers").default;
 const assert = require("node:assert");
 const path = require("node:path");
@@ -11,31 +11,40 @@ let successes = 0;
 
 const MAX_TIME = 1000;
 const CONCURRENCY = 200;
+const BATCH_SIZE = 20000;
 const WORKERS = 4;
 
 async function validate({ handle, cache, cacheRef }) {
-    const { hash } = await handle({ cacheRef });
+    const { hash } = await handle();
     console.log(`${performance.now()}: Getting data with key ${hash}`);
     let buffer;
     let retried = false;
-    try {
-        buffer = await cache.getBlob(hash);
-    } catch (e) {
-        await new Promise(resolve => {
-            setTimeout(() => {
-                console.log(`${performance.now()}: Retrying data with key ${hash}`);
-                cache.getBlob(hash).then(v => {
-                    buffer = v;
-                    retried = true;
-                    retries++;
-                    resolve();
-                });
-            }, 0);
-        });
-        
+    // await new Promise(resolve => setTimeout(resolve, 0));
+    buffer = cache.get(hash);
+    if (buffer == null) {
+        try {
+            await new Promise((resolve, reject) => {
+                // setTimeout(() => {
+                    console.log(
+                        `${performance.now()}: Retrying data with key ${hash}`
+                    );
+                    buffer = cache.get(hash);
+                    if (buffer == null) {
+                        reject(
+                            new Error(`Still failed to get ${hash} after retry`)
+                        );
+                        return;
+                    }
+                        retries += 1;
+                        retried = true;
+                        resolve();
+            });
+        } catch (e) {
+            console.log(e.message);
+        }
     }
     assert(buffer.length === 10000);
-    successes++;
+    successes += 1;
     // if (retried) {
     //     throw new Error(`Worked only after a retry ${hash}`);
     // }
@@ -44,7 +53,11 @@ async function validate({ handle, cache, cacheRef }) {
 
 async function main() {
     await rimraf("./cache");
-    const cache = new LMDBCache("./cache");
+    const cache = db.open("./cache", {
+        compression: true,
+        encoding: "binary",
+        name: "parcel-cache",
+    });
     logger.onLog(async (event) => {
         if (event.level === "error") {
             console.log(await prettyDiagnostic(event.diagnostics[0]));
@@ -64,7 +77,6 @@ async function main() {
         shouldPatchConsole: false,
     });
 
-    const { ref: cacheRef } = await farm.createSharedReference(cache);
     const handle = await farm.createHandle("run", false);
 
     // Produce requests in batches until we crash.. :P
@@ -73,8 +85,8 @@ async function main() {
             maxConcurrent: CONCURRENCY,
         });
 
-        for (let i = 0; i < 20000; i++) {
-            queue.add(() => validate({ handle, cache, cacheRef }));
+        for (let i = 0; i < BATCH_SIZE; i++) {
+            queue.add(() => validate({ handle, cache }));
         }
 
         await queue.run();
--- worker.js
+++ worker.js
@@ -1,28 +1,34 @@
-
 const { Buffer } = require("node:buffer");
-const { hashBuffer } = require("@parcel/hash");
+const { createHash } = require("node:crypto");
 const { performance } = require("node:perf_hooks");
+const db = require("lmdb");
 
 console.log(`${performance.now()}: worker init`);
 
-// register with serializer
-require('@parcel/cache');
-
 const BLOB_SIZE = 10000;
 
-async function run(workerApi, { cacheRef }) {
-  const cache = workerApi.getSharedReference(cacheRef);
-  // Generate a "large" blob
-  const data = [];
-  for (let i = 0; i < 10000; i++) {
-    data.push(Math.floor(Math.random() * 255));
-  }
-  const blob = Buffer.from(data);
-  const hash = hashBuffer(blob);
-  console.log(`${performance.now()}: Setting data with key ${hash}`);
-  await cache.setBlob(hash, blob);
-  console.log(`${performance.now()}: Committed data with key ${hash}`);
-  return { hash };
+async function run() {
+    const cache = db.open("./cache", {
+        compression: true,
+        encoding: "binary",
+        name: "parcel-cache",
+    });
+    // Generate a "large" blob
+    const data = [];
+    for (let i = 0; i < 10000; i++) {
+        data.push(Math.floor(Math.random() * 255));
+    }
+    const blob = Buffer.from(data);
+    const hash = createHash("md5");
+    hash.update(blob);
+    const key = hash.digest("hex");
+    console.log(`${performance.now()}: Setting data with key ${key}`);
+    let result = await cache.put(key, blob);
+    if (!result) {
+      throw new Error(`Write to cache failed for ${key}`);
+    }
+    console.log(`${performance.now()}: Committed data with key ${key}`);
+    return { hash: key };
 }
 
 module.exports = { run };
